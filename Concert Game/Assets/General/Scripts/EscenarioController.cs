e1bafae618d3890c5d53b30b51c655579a28f2fa
TPA-DED <danielasalas3434@gmail.com>
2026-07-27 13:39:27 -0600

    Cambios en Reflectores y Escenarios - Se añadió lo de Dani

--- Concert Game/Assets/General/Scripts/EscenarioController.cs
+++ Concert Game/Assets/General/Scripts/EscenarioController.cs
@@ -1,18 +1,99 @@
+//using UnityEngine;
+
+//public class EscenarioController : MonoBehaviour
+//{
+//    public ReflectorController[] reflectores;
+//    private int reflectorSeleccionado = -1;
+
+//    void Start()
+//    {
+//        //Debug.Log("ENTRA");
+//    }
+
+//    void Update()
+//    {
+//        for (int i = 0; i < reflectores.Length; i++)
+//        {
+//            if(Input.GetKeyDown(KeyCode.Alpha1 + i)) { 
+//                reflectorSeleccionado = i;
+//                Debug.Log("Reflector" + (i + 1) + "seleccionado");
+//            }
+//        }
+
+//        if (reflectorSeleccionado >= 0 && Input.GetKeyDown(KeyCode.E))
+//        {
+//            reflectores[reflectorSeleccionado].Toggle();
+//        }
+
+//        if (Input.GetKeyDown(KeyCode.Escape))
+//        {
+//            reflectorSeleccionado = -1;
+//            Debug.Log("Ningún reflector se encuentra seleccionado");
+//        }
+
+//        if (reflectorSeleccionado >= 0)
+//        {
+//            float mouseX = Input.GetAxis("Mouse X");
+//            float mouseY = Input.GetAxis("Mouse Y");
+//            reflectores[reflectorSeleccionado].Rotate(mouseX, mouseY);
+//        }
+//    }
+//}
+
 using UnityEngine;
 
 public class EscenarioController : MonoBehaviour
 {
     public ReflectorController[] reflectores;
+    private int reflectorSeleccionado;
+    private bool toggleReflector = false;
+    //public BocinaController bocina;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //Debug.Log("ENTRA");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            toggleReflector = !toggleReflector;
+            if (!toggleReflector)
+            {
+                reflectorSeleccionado = -1;
+            }
+            Debug.Log("Entra");
+        }
+
+        if (toggleReflector)
+        {
+            for (int i = 0; i < reflectores.Length; i++)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+                {
+                    reflectorSeleccionado = i;
+                    Debug.Log("Reflector" + (i + 1) + "seleccionado");
+                }
+            }
+
+            if (reflectorSeleccionado >= 0 && Input.GetKeyDown(KeyCode.E))
+            {
+                reflectores[reflectorSeleccionado].Toggle();
+            }
+
+            if (reflectorSeleccionado >= 0)
+            {
+                float mouseX = Input.GetAxis("Mouse X");
+                float mouseY = Input.GetAxis("Mouse Y");
+                reflectores[reflectorSeleccionado].Rotate(mouseX, mouseY);
+            }
+        }
+
+        //if (Input.GetKeyDown(KeyCode.R))
+        //{
+        //    bocina.Toggle();
+        //}
     }
 }
+
